d655b6b3b25995e7a341661c13e61b230c80a515
Armstrong Omoregie <ammycan6@gmail.com>
2024-05-12 17:25:09 +0100

    update2

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => (
     </div>
 
     <div className='app__wrapper_img'>
-      <img src={images.welcome} alt="header image" />
+      <img src={images.welcome} alt="image" />
     </div>
   </div>
 );
